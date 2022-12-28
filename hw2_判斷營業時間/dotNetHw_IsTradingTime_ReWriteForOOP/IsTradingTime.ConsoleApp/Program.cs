7da1c015f714587d05fd2c26aeb1ae819a8db43b
TTYeh <35258945+TTYeh@users.noreply.github.com>
2022-12-28 15:31:55 +0800

    add 作業

---
+++ hw2_判斷營業時間/dotNetHw_IsTradingTime_ReWriteForOOP/IsTradingTime.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using IsTradingTime.ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsTradingTime.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StockLibrary.IsTrading(Convert.ToDateTime("2022/12/23 9:30:01")));
+            Console.ReadLine();
+        }
+    }
+}
